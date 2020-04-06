d301f0c7c35773deaa1812bcc7c044ee69238116
Jose Luis Camacho <jose.luis244@gmail.com>
2020-04-06 15:48:13 -0400

    a

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(fileUpload({
 
 app.use(cors())
 app.use(express.json())
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.urlencoded({extended: true}));
 app.use('/',Rutas)
 app.use('/servicio',Servicio)
 
