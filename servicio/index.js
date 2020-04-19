186da905cbf255c8bce19c9e39c919c9688c15db
Jose Luis Camacho <jose.luis244@gmail.com>
2020-04-19 17:22:09 -0400

    espacio

--- servicio/index.js
+++ servicio/index.js
@@ -2,6 +2,8 @@ const express = require("express")
 const app = express()
 const Usuario = require("../modelos/usuarios")
 const ListaCliente = require("../modelos/listaclientes")
+const checkDiskSpace = require('check-disk-space')
+const fs = require("fs")
 ///////////////////////////////
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/medpacs', {useNewUrlParser: true});
@@ -27,5 +29,17 @@ app.post("/agregarlista",async (req,res)=>{
     console.log(Ncliente)
     res.send(true)
 })
+app.get("/getspace",(req,res)=>{
+    let DiskRuta = fs.readFileSync("./disk.json")
+    DiskRuta = JSON.parse(DiskRuta.toString())
+    checkDiskSpace(DiskRuta.ruta).then((diskSpace) => {
+        let Pocupado = (100*(diskSpace.size-diskSpace.free))/(diskSpace.size)
+        res.json({uso:Pocupado.toFixed(2)})
+        // {
+        //     free: 12345678,
+        //     size: 98756432
+        // }
+    })
 
+})
 app.listen(8000)
