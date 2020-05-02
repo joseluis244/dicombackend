8d203e71c3ad10df42c2f49e4ec8d3057693442e
Jose Luis Camacho <jose.luis244@gmail.com>
2020-05-02 11:21:57 -0400

    ssl

--- index.js
+++ index.js
@@ -4,25 +4,25 @@ const cors = require("cors");
 const Rutas = require('./Rutas');
 const administracion = require("./administracion/Rutas")
 const fs = require("fs")
-//const https = require("https")
-//const https_port = 4443
+const https = require("https")
+const https_port = 4443
 
-//const secureserveroptios = {
-//    key:fs.readFileSync("/var/www/html/medpacs/ssl/private.key"),
-//    cert:fs.readFileSync("/var/www/html/medpacs/ssl/certificate.crt")
-//}
+const secureserveroptios = {
+    key:fs.readFileSync("/var/www/html/medpacs/ssl/private.key"),
+    cert:fs.readFileSync("/var/www/html/medpacs/ssl/certificate.crt")
+}
 
-//https.createServer(secureserveroptios, app)
-//.listen(https_port,()=>{
-//    console.log("secure server enable")
-//})
+https.createServer(secureserveroptios, app)
+.listen(https_port,()=>{
+    console.log("secure server enable")
+})
 
 
 app.use(cors())
 
-//app.use(fileUpload({
-//    createParentPath: true
-//}));
+app.use(fileUpload({
+    createParentPath: true
+}));
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}));
